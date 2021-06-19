d604463554d8f8ea13aa4095707bff6c8494080d
joschiservice <joschua.hass.sh@gmail.com>
2021-06-19 20:42:38 +0200

    adjusted widget corner size and height

--- VTCManager Client/UI/Views/Models/MainDashboardViewModel.cs
+++ VTCManager Client/UI/Views/Models/MainDashboardViewModel.cs
@@ -8,6 +8,8 @@ namespace VTCManager_Client.UI.Views.Models
 {
     public class MainDashboardViewModel : INotifyPropertyChanged
     {
+        public uint WidgetsCornerRadius = 20;
+
         public String SpeedLimitString
         {
             get { return _SpeedLimitString; }
