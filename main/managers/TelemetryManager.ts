a9371e5b8385abf184b55e49d14e8c370524482f
Joschua H <joschua.hass.sh@gmail.com>
2021-12-03 15:57:57 +0100

    handle job started event

--- main/managers/TelemetryManager.ts
+++ main/managers/TelemetryManager.ts
@@ -1,16 +1,19 @@
-import Telemetry, {TelemetryData} from "trucksim-telemetry";
+import Telemetry, {EventsJobStartedVerbose, TelemetryData, Telemetry as TelemetryType} from "trucksim-telemetry";
 import Log from "@awesomeeng/awesome-log";
 import {BrowserWindow} from "electron";
 
 export class TelemetryManager {
-    static telemetry;
+    static telemetry: TelemetryType;
     static readonly UpdateInterval = 50;
     static appWindow : BrowserWindow;
 
     static Init(mainWindow: BrowserWindow) {
         TelemetryManager.appWindow = mainWindow;
         TelemetryManager.telemetry = Telemetry();
+
         TelemetryManager.UpdateReceived(null);
+        TelemetryManager.telemetry.job.on("started", this.JobStarted);
+
         TelemetryManager.telemetry.watch({interval: TelemetryManager.UpdateInterval}, TelemetryManager.UpdateReceived);
         Log.info("[Telemetry] Listening to telemetry updates with an interval of " + TelemetryManager.UpdateInterval);
     }
@@ -25,5 +28,10 @@ export class TelemetryManager {
             active,
             data
         });
+    }
+
+    static JobStarted(data: EventsJobStartedVerbose) {
+        Log.info("[Telemetry] A new job has been started!");
+        Log.debug("[Telemetry] Job Data: " + JSON.stringify(data));
     }
 }
