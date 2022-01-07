d729030be14e07efb2748f9875cf311272576bda
Joschua H <joschua.hass.sh@gmail.com>
2022-01-07 09:41:29 +0100

    fixed updater maybe

--- main/managers/UpdateManager.ts
+++ main/managers/UpdateManager.ts
@@ -1,15 +1,20 @@
-import { autoUpdater } from "electron-updater";
+import { NsisUpdater  } from "electron-updater";
 
 export class UpdateManager {
     static IsUpdating = false;
 
     static async Init() {
+        const appUpdater = new NsisUpdater({
+            provider: 'generic',
+            url: 'https://hazel-sage-zeta.vercel.app'
+        });
+
         const log = require("electron-log");
         log.transports.file.level = "debug";
-        autoUpdater.logger = log;
+        appUpdater.logger = log;
 
         // result is null in dev environment
-        let result = await autoUpdater.checkForUpdatesAndNotify(); // ToDO: investigate result of update check
+        let result = await appUpdater.checkForUpdatesAndNotify(); // ToDO: investigate result of update check
         if (result) {
             console.log("got: " + result);
             this.IsUpdating = true;
