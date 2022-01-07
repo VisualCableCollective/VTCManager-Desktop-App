29b879be517c71ded67071d23fd8b990fb4e04ce
Joschua H <joschua.hass.sh@gmail.com>
2022-01-07 09:12:42 +0100

    edited publish config

--- main/managers/UpdateManager.ts
+++ main/managers/UpdateManager.ts
@@ -1,5 +1,4 @@
 import { autoUpdater } from "electron-updater";
-import { app } from "electron";
 
 export class UpdateManager {
     static IsUpdating = false;
@@ -8,37 +7,6 @@ export class UpdateManager {
         const log = require("electron-log");
         log.transports.file.level = "debug";
         autoUpdater.logger = log;
-
-        // redirect to custom server
-        const server = "https://hazel-sage-zeta.vercel.app";
-        const url = `${server}/update/${process.platform}/${app.getVersion()}`
-
-        autoUpdater.setFeedURL({
-            acl: undefined,
-            channel: undefined,
-            channels: undefined,
-            component: undefined,
-            distribution: undefined,
-            encryption: undefined,
-            endpoint: undefined,
-            host: undefined,
-            package: undefined,
-            path: undefined,
-            platform: undefined,
-            private: undefined,
-            protocol: undefined,
-            provider: undefined,
-            publishAutoUpdate: false,
-            publisherName: undefined,
-            releaseType: undefined,
-            requestHeaders: undefined,
-            storageClass: undefined,
-            updateProvider: undefined,
-            updaterCacheDirName: undefined,
-            useMultipleRangeRequest: false,
-            user: undefined,
-            vPrefixedTagName: false,
-            url: url })
 
         // result is null in dev environment
         let result = await autoUpdater.checkForUpdatesAndNotify(); // ToDO: investigate result of update check
