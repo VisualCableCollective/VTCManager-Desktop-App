cc52b09a713fc586b7f28303d45d7b3a3225240d
Joschua H <joschua.hass.sh@gmail.com>
2022-12-02 14:32:16 +0000

    v1.5.6

--- VTCManager Client/AppInfo.cs
+++ VTCManager Client/AppInfo.cs
@@ -7,17 +7,17 @@
     {
         public static readonly string AppName = "VTCManager";
         public static readonly string Author = "The VisualCable Collective";
-        public static readonly string VersionId = "1.5.5";
+        public static readonly string VersionId = "1.5.6";
         public static readonly string Version = $"v{VersionId} Public Beta";
         public static readonly string DiscordClientID = "659036297561767948";
 
         #region UpdateInfo
-        public static readonly string UpdatePublishedDate = "November 07, 2022";
+        public static readonly string UpdatePublishedDate = "December 2, 2022";
 
         //Changelog
         public static readonly string CLNewFeaturesList = "";
         public static readonly string CLAdditionalImprovementsList = "";
-        public static readonly string CLSecurityAndBugFixesList = "Fixed an issue, that caused a not user-friendly error message, when starting a new job without being a member of a company.\nFixed an issue, that showed an error message that the job couldn't be finished properly, even though everything was fine.";
+        public static readonly string CLSecurityAndBugFixesList = "Fixed an issue, that caused a not user-friendly error message, when starting a new job without being a member of a company.\nFixed an issue, that showed an error message that the job couldn't be finished properly, even though everything was fine.\n-Fixed an issue, that prevented the client from launching, when neither ETS or ATS is installed.";
         #endregion
 
         /// <summary>
